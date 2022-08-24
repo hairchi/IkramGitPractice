dc3d83a00b70a15585229210e16b7de064cdac64
Ikram Hairchi <hairchi1998@gmail.com>
2022-08-24 12:35:15 -0400

    login edited

--- src/testCae1/login.java
+++ src/testCae1/login.java
@@ -2,7 +2,7 @@ package testCae1;
 
 public class login {
     public static void main(String[] args) {
-        System.out.println("name");
+
         String name= "";
         String lastName= "";
         if(name.equals("Sandia") && lastName.equals("taj")) {

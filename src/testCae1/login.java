0134870472ae4ca6000ae300c14d7995c6d2749a
Ikram Hairchi <hairchi1998@gmail.com>
2022-08-24 12:33:44 -0400

    login created

--- src/testCae1/login.java
+++ src/testCae1/login.java
@@ -3,6 +3,11 @@ package testCae1;
 public class login {
     public static void main(String[] args) {
         System.out.println("name");
+        String name= "";
+        String lastName= "";
+        if(name.equals("Sandia") && lastName.equals("taj")) {
+            System.out.println("successful login");
+        }
 
     }
 }

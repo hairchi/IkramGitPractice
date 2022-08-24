8da6d67b2a509db830ea5eeda4090ac1d6753d28
Ikram Hairchi <hairchi1998@gmail.com>
2022-08-24 12:46:52 -0400

    login created

---
+++ src/testCase1/Login.java
@@ -0,0 +1,18 @@
+
+    package testCase1;
+
+    public class Login {
+        public static void main(String[] args) {
+
+            String name = "";
+            String lastName = "";
+            if (name.equals("Sandia") && lastName.equals("taj")) {
+                System.out.println("successful login");
+
+            }
+
+        }
+    }
+
+
+
